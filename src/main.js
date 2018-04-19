e92ad6589a3cdc98c8b548db08fe4cedf870e450
a712718 <1023940468@qq.com>
2018-04-19 20:44:33 +0800

    element-ui分模块加载

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './vuex'
-import ElementUI from 'element-ui'
+import {Menu, MenuItem, Row, Col, Card, Button} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Card)
+Vue.use(Button)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
